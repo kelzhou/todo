10d6c5e6a0c73ad86f0ce1f8dc769cd1ee910b24
Kelly Zhou <kelzhou3@gmail.com>
2013-10-04 02:45:09 -0400

    Added user association to assignments through migration

---
+++ db/migrate/20131004062426_add_user_to_assignments.rb
@@ -0,0 +1,5 @@
+class AddUserToAssignments < ActiveRecord::Migration
+  def change
+  	add_column :assignments, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130928052336) do
+ActiveRecord::Schema.define(:version => 20131004062426) do
 
   create_table "assignments", :force => true do |t|
     t.string   "class_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130928052336) do
     t.string   "due_date"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
