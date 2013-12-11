7d438ff5926d9b588ed3bbd950f2c97ea5ea2b9f
Kelly Zhou <kelzhou3@gmail.com>
2013-12-11 18:36:25 -0500

    Added descriptions to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,12 +2,13 @@
 #
 # Table name: events
 #
-#  id         :integer          not null, primary key
-#  title      :string(255)
-#  date       :string(255)
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  date        :string(255)
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  description :string(255)
 #
 
 class Event < ActiveRecord::Base
@@ -17,7 +18,6 @@ class Event < ActiveRecord::Base
   validates :user, :title, :date, :presence => true
 
   scope :meeting, -> { where('title = ?', "Meeting")}
-  scope :date, -> { where('title = ?', "Meeting")}
+  scope :date, -> { where('title = ?', "Date")}
   scope :others, lambda { {:conditions => ['title != ? AND title != ?', "Meeting", "Date"]} }
-  # -> { where('title != ?', "Meeting")}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131211030539) do
+ActiveRecord::Schema.define(:version => 20131211233208) do
 
   create_table "assignments", :force => true do |t|
     t.string   "class_name"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(:version => 20131211030539) do
     t.string   "title"
     t.string   "date"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "description"
   end
 
   add_index "events", ["user_id"], :name => "index_events_on_user_id"
